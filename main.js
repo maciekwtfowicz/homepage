28cc640c140bde50cce0e660c84492bc6bb0544e
Maciej Korsan <maciek@cotenfrontend.pl>
2023-07-15 11:59:50 +0200

    BEM & DOM

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const firstName = 'Maciek';
+const firstName = 'Maciek Korsan';
 const age = 35;
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
+
+const header = document.querySelector('.page-header__heading--js')
+
+header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`;
+
+console.log(header.style)
+
+header.style.color = 'red';

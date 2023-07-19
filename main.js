97bab606046fbf946f8b14522c6da698d1f174b5
Maciej Korsan <maciek@cotenfrontend.pl>
2023-07-19 22:56:56 +0200

    typography playground

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
 const header = document.querySelector('.page-header__heading--js')
 
-header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat`;
 
 console.log(header.style)
 

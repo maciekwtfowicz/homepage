01d784eec091488458779eae95982f766123bd4f
Maciej Korsan <maciek@cotenfrontend.pl>
2023-07-21 16:23:38 +0200

    html & js

--- main.js
+++ main.js
@@ -6,10 +6,34 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 const header = document.querySelector('.page-header__heading--js')
 
 
-console.log(header.style)
-
 header.style.color = 'red';
 
 const contentJs = document.querySelector('.my-article__content--js');
 
 contentJs.innerHTML = 'Jestem wypełniony javascriptem  🤯'
+
+const calculate = myNumber => (myNumber+4)*7
+
+
+calculate(1);
+
+const result = calculate(2);
+console.log(result)
+
+
+const deathStar = {
+  diameter: 120000,
+  fire: (target) => {
+    console.log(`${target} destroyed 💥`)
+  },
+  isOperating: true,
+  levels: 357,
+  name: 'Death Star',
+  population: 10000,
+  commander: {
+    name: 'darth vader'
+  }
+ }
+
+ deathStar.fire('alderaan')
+ deathStar.fire('scarif')
